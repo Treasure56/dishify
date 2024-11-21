266b99ff7787c018a44756f56665dda5f1880a0c
treasure56 <simplicitytreasure@gmail.com>
2024-11-21 07:00:19 -0800

    .

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="bg-neutral-100 w-full flex justify-center items-center py-4">
-            <p>Allright reserved Dishify &copy; 2023</p>
+            <p>Allright reserved Dishify &copy; 2024</p>
 
         </div>
     );
